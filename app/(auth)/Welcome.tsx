80f64711e664b819cfe18d2e2dc9267ac31c0c45
sampath kumar <jettysampath8@gmail.com>
2024-10-19 12:54:50 +0530

    responsive for welcome

--- app/(auth)/Welcome.tsx
+++ app/(auth)/Welcome.tsx
@@ -6,65 +6,108 @@ import Swiper from "react-native-swiper";
 import { onboarding } from "../../constants";
 import CustomButton from "../../components/CustomButton";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import {
+  responsiveHeight,
+  responsiveWidth,
+  responsiveFontSize,
+} from "react-native-responsive-dimensions";
 
 export default function Welcome() {
-	const swiperRef = useRef<Swiper>(null);
-	const [activeIndex, setActiveIndex] = useState(0);
-	const isLastSlide = activeIndex === onboarding.length - 1;
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const isLastSlide = activeIndex === onboarding.length - 1;
 
-	const handleNext = () => {
-		if (swiperRef.current && !isLastSlide) {
-			swiperRef.current.scrollBy(1);
-		} else {
-			router.push("/(auth)/Initial");
-		}
-	};
-	return (
-		<SafeAreaView className="flex h-full items-center justify-between bg-white">
-			<TouchableOpacity
-				onPress={() => {
-					router.push("/(root)/(tabs)/Home");
-				}}
-				className="w-full flex justify-end items-end p-5"
-			>
-				<Text className="text-xl text-black/70 font-JakartaBold underline">
-					skip{" "}
-				</Text>
-			</TouchableOpacity>
-			<Swiper
-				ref={swiperRef}
-				loop={false}
-				dot={
-					<View className="w-[32px] h-[4px] mx-1 bg-[#e2e8f0] rounded-full" />
-				}
-				activeDot={
-					<View className="w-[32px] h-[4px] mx-1 bg-[#0286ff] rounded-full" />
-				}
-				onIndexChanged={(index) => setActiveIndex(index)}
-			>
-				{onboarding.map((item, index) => (
-					<View key={index} className="flex items-center justify-center p-5">
-						<Image source={item.image} className="w-full h-[300px]" />
-						<View className="flex flex-row items-center justify-center w-full mt-10">
-							<Text className="text-3xl text-black font-bold mx-10 text-center">
-								{item.title}
-							</Text>
-						</View>
-						<Text className="text-lg font-JakartaSemiBold text-center mx-5 mt-3 text-[#858585]">
-							{item.description}
-						</Text>
-					</View>
-				))}
-			</Swiper>
+  const handleNext = () => {
+    if (swiperRef.current && !isLastSlide) {
+      swiperRef.current.scrollBy(1);
+    } else {
+      router.push("/(auth)/Initial");
+    }
+  };
 
-			<CustomButton
-				title={isLastSlide ? "Get Started" : "Next"}
-				className="w-11/12 mt-10 transition-all duration-1000 ease-in-out"
-				onPress={handleNext}
-				IconRight={() => (
-					<FontAwesome6 name="arrow-right-long" size={30} color="white" />
-				)}
-			/>
-		</SafeAreaView>
-	);
+  return (
+    <SafeAreaView className="flex h-full items-center justify-between bg-white">
+      <TouchableOpacity
+        onPress={() => {
+          router.push("/(root)/(tabs)/Home");
+        }}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-xl text-black/70 font-JakartaBold underline">
+          skip{" "}
+        </Text>
+      </TouchableOpacity>
+      <View className="flex-1 flex flex-row items-center justify-center">
+        <Swiper
+          ref={swiperRef}
+          loop={false}
+          dot={
+            <View
+              className="rounded-full transition-all duration-1000 ease-in-out"
+              style={{
+                width: responsiveWidth(8),
+                height: responsiveHeight(0.5),
+                marginHorizontal: responsiveWidth(1),
+                backgroundColor: "#e2e8f0",
+              }}
+            />
+          }
+          activeDot={
+            <View
+              className=" bg-[#0286ff] rounded-full transition-all duration-1000 ease-in-out"
+              style={{
+                width: responsiveWidth(8),
+                height: responsiveHeight(0.5),
+                marginVertical: responsiveWidth(1),
+              }}
+            />
+          }
+          onIndexChanged={(index) => setActiveIndex(index)}
+        >
+          {onboarding.map((item, index) => (
+            <View
+              key={index}
+              className="flex items-center justify-center"
+              style={{
+                marginHorizontal: responsiveWidth(2),
+                padding: responsiveWidth(4),
+              }}
+            >
+              <Image
+                source={item.image}
+                resizeMode="contain"
+                className=""
+                style={{
+                  width: responsiveWidth(100),
+                  height: responsiveHeight(30),
+                  alignSelf: "center",
+                }}
+              />
+              <View className="flex flex-row items-center justify-center w-full mt-10">
+                <Text className="text-3xl text-black font-bold mx-10 text-center">
+                  {item.title}
+                </Text>
+              </View>
+              <Text className="text-lg font-JakartaSemiBold text-center mx-5 mt-3 text-[#858585]">
+                {item.description}
+              </Text>
+            </View>
+          ))}
+        </Swiper>
+      </View>
+
+      <CustomButton
+        title={isLastSlide ? "Get Started" : "Next"}
+        className="w-11/12 mt-10 transition-all duration-1000 ease-in-out"
+        onPress={handleNext}
+        IconRight={() => (
+          <FontAwesome6
+            name="arrow-right-long"
+            size={responsiveFontSize(3)}
+            color="white"
+          />
+        )}
+      />
+    </SafeAreaView>
+  );
 }
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,94 +1,103 @@
 import { Text, TouchableOpacity, ButtonProps } from "react-native";
 import React from "react";
+import {
+  responsiveHeight,
+  responsiveWidth,
+  responsiveFontSize,
+} from "react-native-responsive-dimensions";
 
 type BgVariantProps =
-	| "primary"
-	| "secondary"
-	| "danger"
-	| "outline"
-	| "success"
-	| "default";
+  | "primary"
+  | "secondary"
+  | "danger"
+  | "outline"
+  | "success"
+  | "default";
 
 type TextVariantProps =
-	| "primary"
-	| "danger"
-	| "secondary"
-	| "success"
-	| "default";
+  | "primary"
+  | "danger"
+  | "secondary"
+  | "success"
+  | "default";
 
 type CustomButtonProps = {
-	bgVariant?: BgVariantProps;
-	textVariant?: TextVariantProps;
-	className: string;
-	disabled?: boolean;
-	IconLeft?: React.FC | undefined;
-	IconRight?: React.FC | undefined;
-	onPress: () => void;
-	title: string;
+  bgVariant?: BgVariantProps;
+  textVariant?: TextVariantProps;
+  className: string;
+  disabled?: boolean;
+  IconLeft?: React.FC | undefined;
+  IconRight?: React.FC | undefined;
+  onPress: () => void;
+  title: string;
 };
 
 const getBgVariantStyle = (variant: BgVariantProps) => {
-	switch (variant) {
-		case "secondary":
-			return "bg-gray-500";
-		case "danger":
-			return "bg-red-500";
-		case "outline":
-			return "bg-transparent border-[0.7px] border-neutral-300";
-		case "success":
-			return "bg-green-500";
-		default:
-			return "bg-[#0286ff]";
-	}
+  switch (variant) {
+    case "secondary":
+      return "bg-gray-500";
+    case "danger":
+      return "bg-red-500";
+    case "outline":
+      return "bg-transparent border-[0.7px] border-neutral-300";
+    case "success":
+      return "bg-green-500";
+    default:
+      return "bg-[#0286ff]";
+  }
 };
 
 const getTextVariantStyle = (variant: TextVariantProps) => {
-	switch (variant) {
-		case "primary":
-			return "text-black";
-		case "danger":
-			return "text-red-100";
-		case "secondary":
-			return "text-gray-100";
-		case "success":
-			return "text-green-100";
-		default:
-			return "text-white";
-	}
+  switch (variant) {
+    case "primary":
+      return "text-black";
+    case "danger":
+      return "text-red-100";
+    case "secondary":
+      return "text-gray-100";
+    case "success":
+      return "text-green-100";
+    default:
+      return "text-white";
+  }
 };
 
 const CustomButton = ({
-	onPress,
-	title,
-	bgVariant = "primary",
-	textVariant = "default",
-	IconLeft = undefined,
-	IconRight = undefined,
-	disabled = false,
-	className,
-	...props
+  onPress,
+  title,
+  bgVariant = "primary",
+  textVariant = "default",
+  IconLeft = undefined,
+  IconRight = undefined,
+  disabled = false,
+  className,
+  ...props
 }: CustomButtonProps) => {
-	return (
-		<TouchableOpacity
-			activeOpacity={0.5}
-			onPress={onPress}
-			disabled={disabled}
-			className={`w-full mb-3 p-3 rounded-full flex flex-row justify-center items-end shadow-md shadow-neutral-400/70 ${getBgVariantStyle(
-				bgVariant,
-			)}  ${className}`}
-			{...props}
-		>
-			{IconLeft && <IconLeft />}
-			<Text
-				className={`text-2xl font-bold mr-3 ${getTextVariantStyle(
-					textVariant,
-				)} `}
-			>
-				{title}
-			</Text>
-			{IconRight && <IconRight />}
-		</TouchableOpacity>
-	);
+  return (
+    <TouchableOpacity
+      activeOpacity={0.5}
+      onPress={onPress}
+      disabled={disabled}
+      className={` rounded-full flex flex-row justify-center items-end box-shadow-md shadow-neutral-400/70 ${getBgVariantStyle(
+        bgVariant
+      )}  ${className}`}
+      {...props}
+      style={{
+        width: responsiveWidth(95),
+        margin: responsiveWidth(5),
+        padding: responsiveHeight(1.5),
+      }}
+    >
+      {IconLeft && <IconLeft />}
+      <Text
+        className={`font-bold mr-3 ${getTextVariantStyle(textVariant)} `}
+        style={{ fontSize: responsiveFontSize(2.5) }}
+      >
+        {title}
+      </Text>
+      {IconRight && <IconRight />}
+    </TouchableOpacity>
+  );
 };
 
 export default CustomButton;
